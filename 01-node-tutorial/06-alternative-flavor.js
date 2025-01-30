2a6ac3952beb884629b72f1f933c168d1ac8dc61
0Kareem0 <kimo123kimo321@gmail.com>
2025-01-30 13:22:57 +0200

    better way of exporting

--- 01-node-tutorial/06-alternative-flavor.js
+++ 01-node-tutorial/06-alternative-flavor.js
@@ -1,6 +1,5 @@
-module.exports.items = ['item1', 'item2']
+items = ['item1', 'item2']
 const person = {
   name: 'bob',
 }
-
-module.exports.singlePerson = person
+module.exports = { items, singlePerson: person };
