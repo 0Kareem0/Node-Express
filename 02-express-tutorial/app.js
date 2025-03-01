eafaa7d9f772bfb5d9788e58edcc6e0faf1d5feb
0Kareem0 <kimo123kimo321@gmail.com>
2025-03-01 16:23:52 +0200

    starting express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,25 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express()
+const port = 5000
+
+app.get('/', (req,res) =>{
+    res.status(200).send('Home Page')
+    console.log('user hit the homePage');
+})
+
+
+app.get('/about', (req,res) =>{
+    res.status(200).send('Home About')
+    console.log('user hit the aboutPage');
+})
+
+
+app.all('*', (req,res) => {
+    res.status(404).send('<h1>Resource not found</h1>')
+    console.log('resource not found');
+})
+
+
+app.listen(port,() => {
+    console.log(`Server is listening on port ${port}...`);
+})
