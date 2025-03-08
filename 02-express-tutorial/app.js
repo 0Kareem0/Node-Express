2a3e914225fd02ec07e258c60300fabff5436ca9
0Kareem0 <kimo123kimo321@gmail.com>
2025-03-09 01:42:34 +0200

    v3

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,25 +1,24 @@
-const express = require('express');
+const express = require('express')
 const app = express()
+const path = require('path')
 const port = 5000
 
-app.get('/', (req,res) =>{
-    res.status(200).send('Home Page')
-    console.log('user hit the homePage');
+
+app.use(express.static('./public'))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+// })
+
+
+app.get('*', (req,res) =>{
+    res.status(404).send('resource not found')
 })
 
 
-app.get('/about', (req,res) =>{
-    res.status(200).send('Home About')
-    console.log('user hit the aboutPage');
+app.listen(port, () => {
+    console.log('Server is listening on port 5000....');
+    
 })
 
-
-app.all('*', (req,res) => {
-    res.status(404).send('<h1>Resource not found</h1>')
-    console.log('resource not found');
-})
-
-
-app.listen(port,() => {
-    console.log(`Server is listening on port ${port}...`);
-})
+console.log(path.resolve(__dirname, './navbar-app/index.html'));
