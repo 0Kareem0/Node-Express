f0d762efb6d43796068d78fcb0a3f7d96d1f17f0
0Kareem0 <kimo123kimo321@gmail.com>
2025-03-12 09:49:17 +0200

    v5

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,14 +1,57 @@
-const express = require(`express`)
-const app = express ()
-const { products } = require(`./data`)
-const port = 5000
+const express = require(`express`);
+const app = express()
+const {products} = require(`./data`)
+const port = 3000
 
-app.get(`/` , (req , res) =>{
-    res.send(products)
+app.get(`/`, (req, res) => {
+    res.send(`<h1>Home Page</h1><a href="/api/products">products</a>`)
 })
+
+app.get(`/api/products`, (req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id , name ,image} = product
+        return {id , name , image}
+    })
+    res.json(newProducts)
+})
+
+app.get(`/api/products/:productID`, (req,res)=>{
+    const {productID} = req.params
+    
+    const singleProduct = products.find((product) =>
+        product.id === Number(productID)
+    )
+    if(!singleProduct){
+        return res.status(404).json({message:'product not found'})
+    }
+    res.json(singleProduct)
+})
+
+ 
+app.get(`/api/v1/query`, (req,res)=>{
+const {search , limit} = req.query
+let sortedProducts = [...products]
+
+if(search){
+    sortedProducts = sortedProducts.filter((product)=>{
+        return product.name.startsWith(search)
+    })
+}
+
+    if(limit){
+        sortedProducts = sortedProducts.slice(0 , Number(limit))
+    }
+
+    if(sortedProducts.length < 1){
+        return res.status(200).json({ sucess: true, data: [] })
+    }
+
+    res.status(200).json(sortedProducts)
+
+}
+)
 
 
 app.listen(port, () =>{
     console.log(`Server is listening on port ${port}...`);
-    
 })
