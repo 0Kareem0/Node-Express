600abdc792003060f37d49eb01cda31e0dae20c4
0Kareem0 <kimo123kimo321@gmail.com>
2025-03-24 13:07:18 +0200

    v4

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -4,8 +4,6 @@ const port = 3000
 let { people } = require(`./data`)
 const logger = require('./logger');
 
-
-
 app.disable('etag');
 app.disable('x-powered-by');
 
@@ -14,13 +12,25 @@ app.use(express.static(`./methods-public`))
 
 app.use(express.urlencoded({extended:false}))
 
+app.use(express.json())
 app.get(`/api/people`, (req ,res)=>{
 res.status(200).json({success:true,data:people})
 })
 
-app.post('/login',(req,res)=>{
+app.post(`/api/people`,(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false, msg:`please provide name value`})
+    }
+      res.status(201).json({success:true,person:name})
+})
 
-    res.send('loging....')
+app.post('/login',(req,res)=>{
+const { name } = req.body    
+if(name){
+    return res.status(200).send(`Welcome ${name}`)
+}
+    res.status(401).send('Enter anything u dumb fuck')
 })
 
 app.listen(port, () =>{
