8eaac0525b703521293be0249b30051ba2ea1e4e
0Kareem0 <kimo123kimo321@gmail.com>
2025-04-05 11:32:50 +0200

    v6

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -13,16 +13,32 @@ app.use(express.static(`./methods-public`))
 app.use(express.urlencoded({extended:false}))
 
 app.use(express.json())
+
 app.get(`/api/people`, (req ,res)=>{
 res.status(200).json({success:true,data:people})
 })
+
 
 app.post(`/api/people`,(req,res)=>{
     const {name} = req.body
     if(!name){
         return res.status(400).json({success:false, msg:`please provide name value`})
     }
+
+    if (!isNaN(name)) {
+        return res.status(400).json({ success: false, msg: `name cannot be a number` });
+      }
+
       res.status(201).json({success:true,person:name})
+}) 
+
+
+app.post(`/api/postman/people`,(req,res)=>{
+    const {name} = req.body
+    if(!name){
+        return res.status(400).json({success:false, msg:`please provide name value`})
+    }
+    res.status(201).send({success:true,data:[...people,name]})
 })
 
 app.post('/login',(req,res)=>{
@@ -30,9 +46,35 @@ const { name } = req.body
 if(name){
     return res.status(200).send(`Welcome ${name}`)
 }
-    res.status(401).send('Enter anything u dumb fuck')
+
+    res.status(401).send({success:false,msg:'Enter anything u dumb fuck'})
 })
 
+
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+
+    let person = people.find((person) => person.id === Number(id))
+
+    if (!person) {
+        // If not found, create a new person
+        person = { id: Number(id), name }
+        people.push(person)
+        return res.status(201).json({ success: true, msg: 'Person created', data: people })
+    }
+
+    // If found, update the name
+    const newPeople = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+
+    res.status(200).json({ success: true, msg: 'Person updated', data: newPeople })
+})
 
 
 
